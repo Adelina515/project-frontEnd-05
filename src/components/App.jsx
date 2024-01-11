5b55e6910ac1a7513bb7cf3bd78a0d451ca8d8e4
Adelina515 <adelgodlevska@gmail.com>
2024-01-11 20:54:30 +0200

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+const Diary = lazy(() => import('../pages/Diary'));
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div></div>;
 };
