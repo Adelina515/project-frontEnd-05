d1a0cc70a04dd54a0a2d7ee73eee29acb0193aa1
olia <dovgalyukolya@gmail.com>
2024-01-13 02:35:12 +0300

    DayProducts

---
+++ src/components/dayExercises/DayExercises.js
@@ -0,0 +1,23 @@
+//import css from "./DayExercises.module.css";
+
+//import ExercisesTable from "../ExercisesTable/ExercisesTable";
+//import EmptyList from "../EmptyList/EmptyList";
+
+const DayExercises = () => {
+
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <h3>Execrcises</h3>
+          </div>
+                  {//diaryExercises.length === 0 ? (<EmptyList listName={"exercises"} />) : (<ExercisesTable exercises={diaryExercises} />)
+                  }
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DayExercises;
