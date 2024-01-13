485ccc497b1f141ffb371d4adf4a6af6c162e303
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2024-01-13 17:07:47 +0200

    Added schemas

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,7 +1,23 @@
 import css from "./sign-up-form.module.css";
-// import {Formic,Form,Field,ErrorMessage} from "formik";
 import AuthButton from "../../btn/AuthButton/AuthButton";
 const SignUpForm=()=>{
+    // const initialValues={
+    //     name:"",
+    //     emeil:"",
+    //     password:""
+    // }
+    // const signUpSchema = Yup.object()({
+    //     name: Yup.string()
+    //       .required('Required')
+    //       .min(2, 'Name must be at least 2 characters')
+    //       .max(10, 'Name must be no more than 16 characters'),
+      
+    //     email: Yup.string().matches(/^\w+@[a-zA-Z]+?\.[a-zA-Z]{2,3}$/).email('Invalid email address').required('Required'),
+      
+    //     password: Yup.string()
+    //       .min(6, 'Password must be at least 6 characters')
+    //       .required('Required'),
+    //   });
     return(
         <>
             <form className={css.form}>
