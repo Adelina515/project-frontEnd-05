16b2ab0af5afd1321e0ee7c099362d5ed7e6c864
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2024-01-13 13:42:07 +0200

    Added responsive

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,19 +1,27 @@
 import css from "./welcome-page.module.css";
 import { NavLink } from "react-router-dom";
+import sprite from "../../img/sprite/symbol-defs.svg";
+
 
 const WelcomePage=()=>{
     return(
         <div className={css.container}>
-         <h1 className={css.mainTitle}>Transforming your <span className={css.part}>body</span> shape with Power Pulse</h1>
-         <div className={css.btnList}>
+        <div className={css.wrapper}>
+         <h1 className={css.mainTitle}>Transforming your body shape with Power Pulse</h1>
+          
+          <svg className={css.iconLoop} width="98px" height="35px">
+          <use href ={`${sprite}#icon-loop`}></use>
+          </svg>
+          </div>
+          <div className={css.btnList}>
           <NavLink
-            to="signup"
+            to="/signup"
             className={css.btnSignUp}
           >
             Sign Up
           </NavLink>
           <NavLink
-            to="signin"
+            to="/signin"
             className={css.btnSignIn}
           >
             Sign In
