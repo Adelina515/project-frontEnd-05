4e959f389debc44a721d158175309c858c07295f
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-01-14 21:29:16 +0300

    added display of all categories without pagination with backend(no auth) + redirection on click

--- src/pages/ExercisePage/Exercises.js
+++ src/pages/ExercisePage/Exercises.js
@@ -2,25 +2,26 @@ import React, { useEffect, useState } from 'react';
 import css from './Exercises.module.css';
 import ExerciseHead from './ExerciseHead';
 import axios from 'axios';
+import ExerciseList from './ExerciseList';
 
 function Exercises(props) {
-    const [exCat, setExCat ] = useState("Body parts");
-    const [arr, setArr] = useState([]);
-    axios.defaults.baseURL="https://power-pulse-backend.onrender.com/exercises/"
+  const [exCat, setExCat] = useState('Body parts');
+  const [arr, setArr] = useState([]);
+  axios.defaults.baseURL = 'https://power-pulse-backend.onrender.com/exercises/';
+  axios.defaults.headers.common.Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTQyNDlmODliYmU0YTAyMDg5MTk4ZCIsImlhdCI6MTcwNTI1NjA5NSwiZXhwIjoxNzA1MzM4ODk1fQ.EcF3rAObaMKRYxaPHAev1mOCEIiQ_qI_Kkj4tlZxk74"
+  useEffect(() => {
+    axios.get("filter?filter="+exCat).then(v=>{
+        setArr(v.data.filter(v=>v && v.filter===exCat));
+    })
+  }, [setArr, exCat]);
 
-    useEffect(()=>{
-        
-        axios.get(exCat.replace(" ", "").toLowerCase()).then(v=>setArr(v.data));
-        
-    }, [setArr, exCat])
-    
-    console.log(arr);
-    return (
-        <div>
-            <ExerciseHead exCat={exCat} setExCat={setExCat}/>
-            
-        </div>
-    );
+  console.log(arr);
+  return (
+    <div>
+      <ExerciseHead exCat={exCat} setExCat={setExCat} />
+      <ExerciseList arr={arr} />
+    </div>
+  );
 }
 
 export default Exercises;
