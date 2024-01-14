fa8b666d9dde0c492b1a2a582be57a8deab4c4a4
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2024-01-14 21:32:05 +0300

    made page go blank while fetching

--- src/pages/ExercisePage/Exercises.js
+++ src/pages/ExercisePage/Exercises.js
@@ -7,19 +7,22 @@ import ExerciseList from './ExerciseList';
 function Exercises(props) {
   const [exCat, setExCat] = useState('Body parts');
   const [arr, setArr] = useState([]);
+  const [loading, setLoading]= useState(false);
   axios.defaults.baseURL = 'https://power-pulse-backend.onrender.com/exercises/';
   axios.defaults.headers.common.Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTQyNDlmODliYmU0YTAyMDg5MTk4ZCIsImlhdCI6MTcwNTI1NjA5NSwiZXhwIjoxNzA1MzM4ODk1fQ.EcF3rAObaMKRYxaPHAev1mOCEIiQ_qI_Kkj4tlZxk74"
   useEffect(() => {
+    setArr([]);
+    setLoading(true);
     axios.get("filter?filter="+exCat).then(v=>{
         setArr(v.data.filter(v=>v && v.filter===exCat));
-    })
+    }).finally(()=>setLoading(false))
   }, [setArr, exCat]);
 
   console.log(arr);
   return (
     <div>
       <ExerciseHead exCat={exCat} setExCat={setExCat} />
-      <ExerciseList arr={arr} />
+      {loading?<div>Loading...</div>:<ExerciseList arr={arr} />}
     </div>
   );
 }
