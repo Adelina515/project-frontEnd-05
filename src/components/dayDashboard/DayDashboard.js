8544e87705d0d6c14e1468624744441bf3abe312
olia <dovgalyukolya@gmail.com>
2024-01-15 02:27:32 +0300

    maket

--- src/components/dayDashboard/DayDashboard.js
+++ src/components/dayDashboard/DayDashboard.js
@@ -2,7 +2,7 @@
 import React from 'react';
 //import { useSelector } from 'react-redux';
 import css from './DayDashboard.module.css';
-import StatisticsItem from '../StatisticsItem/StatisticsItem';
+import StatisticsItem from '../StatisticsItem/StatisticsItem.jsx';
 
 
 
