1dfe32884bf827badd07a026fbaba4a86c816b4d
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2024-01-17 18:28:08 +0200

    styles and logout

--- src/components/userCard/UserCard.js
+++ src/components/userCard/UserCard.js
@@ -4,47 +4,48 @@ import css from './UserCard.module.css';
 import { selectUsersAvatar } from '../../redux/UserPageCntrls/selectors.jsx';
 import sprite from '../../img/sprite/symbol-defs.svg';
 import { updateProfileAvatarTh } from '../../redux/UserPageCntrls/UserPageOperations';
+import toast from 'react-hot-toast';
 
-export const UserProfile = ({ name, avatar }) => {
+export const UserProfile = ({ name, avatar, userToken }) => {
   const dispatch = useDispatch();
   const userAvatar = useSelector(selectUsersAvatar);
   const [avatarURL, setAvatarURL] = useState();
 
   useEffect(() => {
-    // При изменении avatar присваиваем новый URL в avatarURL
     setAvatarURL(avatar);
   }, [avatar]);
 
   const avatarUser = (
-    <img
-      src={avatarURL || userAvatar}
-      className={css.photo}
-      width="100%"
-      alt="Avatar"
-    />
+    <img src={avatarURL || userAvatar} className={css.photo} width="100%" alt="Avatar" />
   );
+
   const avatarStandart = (
     <svg fill="#000" width="62" height="62">
       <use href={`${sprite}#icon-user`}></use>
     </svg>
   );
 
-  const handleAvatarChange = event => {
-    const file = event.target.files[0];
-    if (file) {
-      const blob = new Blob([file]);
+  const handleAvatarChange = async (event) => {
+    const avatar = event.target.files[0];
+
+    if (avatar) {
+      const blob = new Blob([avatar]);
       const objectURL = URL.createObjectURL(blob);
       setAvatarURL(objectURL);
-      // Вызываем экшн для обновления аватара в Redux
-      dispatch(updateProfileAvatarTh(file));
+
+      try {
+        await dispatch(updateProfileAvatarTh({ avatar, userToken }));
+        toast.success('Avatar updated successfully!', { duration: 3000 });
+      } catch (error) {
+        toast.error('Error updating avatar. Please try again.', { duration: 3000 });
+        console.error('Error updating avatar:', error);
+      }
     }
   };
 
   return (
     <div className={css.wrapperPage} style={{ backgroundColor: '#040404' }}>
-      <div className={css.avatar}>
-        {avatarURL ? avatarUser : avatarStandart}
-      </div>
+      <div className={css.avatar}>{avatarURL ? avatarUser : avatarStandart}</div>
       <form id="upload-form">
         <input
           type="file"
