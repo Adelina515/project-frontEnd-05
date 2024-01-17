548f0499fb36752cbdf02f8f0188cbf459152497
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2024-01-17 23:56:10 +0200

    post and modal

--- src/components/productsList/productsItem/ProductsItem.jsx
+++ src/components/productsList/productsItem/ProductsItem.jsx
@@ -1,9 +1,12 @@
 import css from './ProductsItem.module.css';
 import svg from '../../../img/sprite/symbol-defs.svg';
 import { useInView } from 'react-intersection-observer';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { handleSuccess } from '../../../redux/products/productsSlice';
 
 export const ProductsListItem = ({ data, openModal }) => {
+  const dispatch = useDispatch();
+
   const userBlood = useSelector(state => state.profile.blood);
   const { calories, category, title, weight, groupBloodNotAllowed } = data;
   const fixedTitle = title[0].toUpperCase() + title.slice(1).toLowerCase();
@@ -11,6 +14,10 @@ export const ProductsListItem = ({ data, openModal }) => {
     threshold: 0.5,
     triggerOnce: true,
   });
+  const handleOpen = () => {
+    openModal(data);
+    dispatch(handleSuccess(false));
+  };
   return (
     <li className={css.card} ref={ref}>
       {inView && (
@@ -35,7 +42,7 @@ export const ProductsListItem = ({ data, openModal }) => {
               <button
                 type="button"
                 className={css.statusBtn}
-                onClick={() => openModal(data)}
+                onClick={handleOpen}
               >
                 Add
                 <svg className={css.svgBtn}>
