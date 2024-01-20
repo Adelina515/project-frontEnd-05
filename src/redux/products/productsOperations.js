5ba6df5e161232ae476520b36b76274b546da7cd
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2024-01-20 13:03:56 +0200

    finish productsPage

--- src/redux/products/productsOperations.js
+++ src/redux/products/productsOperations.js
@@ -1,23 +1,15 @@
-import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTU3YWJjZWI1ZjgyMWQ3MGM4YzQwYiIsImlhdCI6MTcwNTY4NzAyMSwiZXhwIjoxNzA1NzY5ODIxfQ.H8a3b1ofd6QxQS4ur3RMu-Ws6rNLdp0iu-3om3SZNdU';
-axios.defaults.baseURL = 'https://power-pulse-backend.onrender.com/';
-axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-// const params = {
-//   searchQuery: '',
-//   category: '',
-//   allowed: '',
-//   page: 1,
-//   limit: 100,
-// };
+import instance from '../../instance/instance';
+// const token =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTU3YWJjZWI1ZjgyMWQ3MGM4YzQwYiIsImlhdCI6MTcwNTY4NzAyMSwiZXhwIjoxNzA1NzY5ODIxfQ.H8a3b1ofd6QxQS4ur3RMu-Ws6rNLdp0iu-3om3SZNdU';
+// axios.defaults.baseURL = 'https://power-pulse-backend.onrender.com/';
+// axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 
 export const fetchProducts = createAsyncThunk(
   'products/fetchProducts',
   async (params, { rejectWithValue }) => {
     try {
-      const response = await axios.get('/products', params);
-      // console.log(response.data);
+      const response = await instance.get('/products', { params });
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -25,6 +17,6 @@ export const fetchProducts = createAsyncThunk(
   }
 );
 export const fetchCategories = async () => {
-  const response = await axios.get('/products/categories');
+  const response = await instance.get('/products/categories');
   return response.data;
 };
