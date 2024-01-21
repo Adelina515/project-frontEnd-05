88b44a542d4a9f5fd6d118da1af3b85e92a51ee9
olia <dovgalyukolya@gmail.com>
2024-01-21 18:53:05 +0300

    css

--- src/redux/diary/diaryOperations.js
+++ src/redux/diary/diaryOperations.js
@@ -1,19 +1,19 @@
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
-/*import axios from 'axios';*/
+import axios from 'axios';
 import { toast } from 'react-toastify';
-import instance from '../../instance/instance';
+/*import instance from '../../instance/instance';*/
 
 
-/*const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTU2MTlhMzI5M2U1NzJkZDU0Nzg2YyIsImlhdCI6MTcwNTc5NDQ2NiwiZXhwIjoxNzA1ODc3MjY2fQ.lZS6BC7P1YXTuTFVgzvltJb8224i5FNidbS6dlDqJZg'
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTU2MTlhMzI5M2U1NzJkZDU0Nzg2YyIsImlhdCI6MTcwNTgzMDUxNywiZXhwIjoxNzA1OTEzMzE3fQ.T7bj4BmvPTd2a-y3EW3W9BKNqBRQnVp_4c7I_0_B6io'
 axios.defaults.baseURL = 'https://power-pulse-backend.onrender.com/';
-axios.defaults.headers.common.Authorization = `Bearer ${token}`;*/
+axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 
 export const fetchAllDiary = createAsyncThunk(
   'fetchAllDiary',
   async (date, { rejectWithValue }) => {
     try {
-      const  response  = await instance.get(`/diary/${date}`);
+      const  response  = await axios.get(`/diary/${date}`);
       console.log(response.data);
       return response.data;
     } catch (error) {
@@ -32,7 +32,7 @@ export const addProductDiary = createAsyncThunk(
   'addProductDiary',
   async (productDetails, { rejectWithValue }) => {
     try {
-      const {data} = await instance.post('/diary/products', productDetails);
+      const {data} = await axios.post('/diary/products', productDetails);
       console.log('productDetailsAdd', productDetails);
       toast.success(`Product successfully added to diary!`);
       return data;
@@ -47,7 +47,7 @@ export const deleteProductDiary = createAsyncThunk(
   'deleteProductDiary',
   async ( productId, { rejectWithValue }) => {
     try {
-      const { data } = await instance.delete(`/diary/products/${productId}`);
+      const { data } = await axios.delete(`/diary/products/${productId}`);
       return data;
     } catch (error) {
       toast.error('Oops... Something went wrong! Try again!');
@@ -61,7 +61,7 @@ export const addExercisesDiary = createAsyncThunk(
   async (exerciseDetails,  { rejectWithValue }) => {
    
     try {
-      const { data } = await instance.post('/diary/exercises', exerciseDetails);
+      const { data } = await axios.post('/diary/exercises', exerciseDetails);
       return data;
     } catch (error) {
      toast.error('Oops... Something went wrong! Try again!');
@@ -74,7 +74,7 @@ export const deleteExercisesDiary = createAsyncThunk(
   async (exerciseId, { rejectWithValue }) => {
 
     try {
-      const { data } = await instance.delete(`/diary/exercises/${exerciseId}`);
+      const { data } = await axios.delete(`/diary/exercises/${exerciseId}`);
       return data;
     } catch (error) {
       toast.error('Oops... Something went wrong! Try again!');
