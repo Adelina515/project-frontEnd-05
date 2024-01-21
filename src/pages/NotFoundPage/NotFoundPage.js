cc80417ef6b8b28f633458dd314b6e16cf453901
Adelina515 <adelgodlevska@gmail.com>
2024-01-22 01:01:13 +0200

    add css to notFoundPage

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,9 +1,27 @@
+import { NavLink } from 'react-router-dom';
 import css from './NotFoundPage.module.css';
+import { selectIsUserLogin } from '../../redux/auth/auth-selectors';
+import { useSelector } from 'react-redux';
 
 export default function NotFoundPage() {
+  const isLogin = useSelector(selectIsUserLogin);
   return (
     <div className={css.notfound}>
-      <div className={css.wrapperText}></div>
+      <div className={css.wrapperText}>
+        <h2 className={css.title}>404</h2>
+        <p className={css.text}>
+          Sorry, you have reached a page that we could not find. It seems that
+          you are lost among the numbers and letters of our virtual space.
+          Perhaps this page went on vacation or decided to disappear into
+          another dimension. We apologize for this inconvenience.
+        </p>
+        <NavLink
+          className={css.btnHome}
+          to={!isLogin ? '/profile' : '/welcome'}
+        >
+          Go Home
+        </NavLink>
+      </div>
       <div className={css.wrapperPhoto}></div>
     </div>
   );
