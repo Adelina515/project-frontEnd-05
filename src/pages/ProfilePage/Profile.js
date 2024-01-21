e8f3e74d72d1488603cbc264769878211aae9bb0
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2024-01-21 21:51:49 +0200

    fix errors

--- src/pages/ProfilePage/Profile.js
+++ src/pages/ProfilePage/Profile.js
@@ -3,46 +3,46 @@ import { Daily } from 'components/daily/Daily';
 import { UserProfile } from 'components/userCard/UserCard';
 import { UserForm } from 'components/userForm/UserForm';
 
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { LogOutBtn } from 'btn/logOutBtn/LogOutBtn';
 // import { selectIsUserLogin } from '../redux/auth/auth-selectors';
-import { logout } from '../../servises/api/auth';
+// import { logout } from '../../servises/api/auth';
 
 import {selectUserToken} from '../../redux/UserPageCntrls/selectors';
 
 import css from './ProfilePage.module.css'
 
 import { ExclamationMark } from 'components/exclamation/Exclamation';
-import { current } from '../../redux/auth/auth-operations';
-import { useEffect } from 'react';
+// import { current } from '../../redux/auth/auth-operations';
+// import { useEffect } from 'react';
 
 export default function Profile() {
-  const dispatch = useDispatch();
+// const dispatch = useDispatch();
   const state = useSelector(state => state.auth.user);
   console.log(state, "state in profile")
   const userToken = useSelector(selectUserToken);
 
-  const onLogout = () => {
-    dispatch(logout());
-  };
+  // const onLogout = () => {
+  //   dispatch(logout());
+  // };
 
-    useEffect(() => {
+  //   useEffect(() => {
 
-      const fetchData = async () => {
-        try {
-          await dispatch(current());
-      //    console.log(currentUserData, 'currentUserData');
-          // Обновляем состояние после получения данных
-          // handleProfileUpdate(currentUserData);
-        } catch (error) {
-          console.error('Ошибка при получении данных пользователя:', error);
-          // Добавьте обработку ошибки, например, отображение сообщения об ошибке или перенаправление на страницу входа
-        }
-      };
+  //     const fetchData = async () => {
+  //       try {
+  //         await dispatch(current());
+  //     //    console.log(currentUserData, 'currentUserData');
+  //         // Обновляем состояние после получения данных
+  //         // handleProfileUpdate(currentUserData);
+  //       } catch (error) {
+  //         console.error('Ошибка при получении данных пользователя:', error);
+  //         // Добавьте обработку ошибки, например, отображение сообщения об ошибке или перенаправление на страницу входа
+  //       }
+  //     };
 
-      fetchData(userToken);
+  //     fetchData(userToken);
 
-  },[dispatch, userToken]);
+  // },[dispatch, userToken]);
 
   // Показывать заглушку или другой контент, если данных нет
   if (!state) {
@@ -74,7 +74,7 @@ export default function Profile() {
         />
       </div>
       <ExclamationMark />
-      <LogOutBtn onLogout={onLogout} /></div>
+      <LogOutBtn/></div>
       <UserForm
         userToken={userToken}
         name={state.name}
